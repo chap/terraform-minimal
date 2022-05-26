613bf0f007fdb5482857c7225ed2f2dc0f035c5b
Chap Ambrose <cambrose@mulesoft.com>
2022-05-26 14:30:04 -0500

    Create minimal.tf

---
+++ minimal.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.20.0"
+    }
+  }
+  required_version = ">= 1.1"
+}
+
+resource "aws_route53_zone" "deploy_sandbox" {
+  name     = "deploy.sandbox.msap.io"
+}
+
+locals {
+  timestamp = formatdate("YYYY-MM-DDThh:mm:ssZ", timestamp())
+}
+
+resource "aws_route53_record" "smoketest_txt" {
+  zone_id = aws_route53_zone.deploy_sandbox.zone_id
+  name = ""
+  type = "TXT"
+  ttl = 60
+
+  records = [
+    "${local.timestamp}"
+  ]
+}
